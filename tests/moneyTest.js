b5f82502254eea123af7ea1d8ea719bc9b72f67a
lamaDayoub <lama1e2dayoub@gmail.com>
2026-08-06 22:46:01 +0300

    create autoated tests for formatCurrency function

---
+++ tests/moneyTest.js
@@ -0,0 +1,34 @@
+import { formatCurrency } from '../scripts/utils/money.js';
+
+console.log('test suite: format currency function');
+
+//Basic test cases for the formatCurrency function
+console.log('convert cents into dollars')
+if (formatCurrency(2059) === '20.59') {
+    console.log('pased');
+} else {
+    console.log('failed');
+}
+
+
+// Edge cases 
+console.log('works with 0')
+if (formatCurrency(0) === '0.00') {
+    console.log('pased');
+} else {
+    console.log('failed');
+}
+
+console.log('rounds up to the nearest dollar')
+if (formatCurrency(2000.5) === '20.01') {
+    console.log('pased');
+} else {
+    console.log('failed');
+}
+
+console.log('rounds down to the nearest dollar')
+if (formatCurrency(2000.4) === '20.00') {
+    console.log('pased');
+} else {
+    console.log('failed');
+}
