594e16b6181391fbd6de8bf890970b4050e174da
lamaDayoub <lama1e2dayoub@gmail.com>
2026-08-11 23:36:35 +0300

    create automated tests for orderSummary

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { renderOrderSummary } from "./checkout/oredrSummary.js";
+import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeaderHTML } from "./checkout/checkoutHeader.js";
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
